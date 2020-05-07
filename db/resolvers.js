768f4f85f409ae2f2547c8f91585de7c8ee5aae6
Nahuel Krowicki <nahuelkrowicki@gmail.com>
2020-05-07 17:47:36 -0300

    Database connected

---
+++ db/resolvers.js
@@ -0,0 +1,8 @@
+// Resolvers (Siempre son un objecto)
+const resolvers = {
+    Query: {
+        obtenerCurso: () => "Algo"
+    }
+}
+
+module.exports = resolvers;
---
+++ index.js
@@ -0,0 +1,20 @@
+const { ApolloServer, gql } = require('apollo-server');
+const typeDefs = require('./db/schema')
+const resolvers = require('./db/resolvers')
+
+const conectarDB = require ('./config/db')
+
+// Conectar a la base de datos
+conectarDB();
+
+// Servidor
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+
+// Arrancar el servidor
+server.listen().then(({ url }) => {
+    console.log(`Servidor listo en url: ${url}`)
+}) 
